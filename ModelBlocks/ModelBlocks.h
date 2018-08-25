2c4b03e4d905117bb1509b74cf70fa988079a6f7
yu840915 <yu840915@gmail.com>
2018-08-25 15:21:24 +0800

    Set up

---
+++ ModelBlocks/ModelBlocks.h
@@ -0,0 +1,19 @@
+//
+//  ModelBlocks.h
+//  ModelBlocks
+//
+//  Created by 立宣于 on 2018/8/25.
+//  Copyright © 2018 立宣于. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModelBlocks.
+FOUNDATION_EXPORT double ModelBlocksVersionNumber;
+
+//! Project version string for ModelBlocks.
+FOUNDATION_EXPORT const unsigned char ModelBlocksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModelBlocks/PublicHeader.h>
+
+
